d3a73a10cb56b196201c251bc98e2a1b384e409c
iltafali76-glitch <iltafali76@gmail.com>
2025-12-25 21:47:03 +1100

    Initialize Express app with basic route

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("App is running successfully 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
